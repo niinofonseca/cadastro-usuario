2dca72ceaac85e2db3e5c5abadff0ff52cee0df3
Camila Ester <camilaesterfonseca@gmail.com>
2025-10-04 22:36:43 -0300

    Definição de exceções

--- cadastro-usuario/src/main/java/com/camilaester/cadastro_usuario/business/UsuarioService.java
+++ cadastro-usuario/src/main/java/com/camilaester/cadastro_usuario/business/UsuarioService.java
@@ -21,5 +21,22 @@ public class UsuarioService {
                 () -> new RuntimeException("Email não encontrado")
         );
     }
+
+    public void deletarUsuarioPorEmail(String email) {
+        repository.deleteByEmail(email);
+    }
+
+    public void atualizarUsuarioPorId(Integer id, Usuario usuario) {
+        Usuario usuarioEntity = repository.findById(id).orElseThrow(
+                () -> new RuntimeException("Usuario não encontrado"));
+        Usuario usuarioAtualizado = Usuario.builder()
+                .email(usuario.getEmail() != null ?
+                        usuario.getEmail() : usuarioEntity.getEmail())
+                .nome(usuario.getNome() != null ?
+                        usuario.getNome() : usuarioEntity.getNome())
+                .id(usuarioEntity.getId())
+                .build();
+        repository.saveAndFlush(usuarioAtualizado);
+    }
 }
 
--- cadastro-usuario/src/main/java/com/camilaester/cadastro_usuario/infrastructure/repository/UsuarioRepository.java
+++ cadastro-usuario/src/main/java/com/camilaester/cadastro_usuario/infrastructure/repository/UsuarioRepository.java
@@ -1,6 +1,7 @@
 package com.camilaester.cadastro_usuario.infrastructure.repository;
 
 import com.camilaester.cadastro_usuario.infrastructure.entitys.Usuario;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
@@ -9,4 +10,6 @@ public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
 
     Optional<Usuario> findByEmail(String email);
 
+    @Transactional
+    void deleteByEmail(String email);
 }
