74a31c325c5e6bc6135e6657f34467b67fb2ab47
Camila Ester <camilaesterfonseca@gmail.com>
2025-10-04 23:12:39 -0300

    Atualização UsuarioController e configuração do properties

--- cadastro-usuario/src/main/java/com/camilaester/cadastro_usuario/controller/UsuarioController.java
+++ cadastro-usuario/src/main/java/com/camilaester/cadastro_usuario/controller/UsuarioController.java
@@ -1,4 +1,44 @@
 package com.camilaester.cadastro_usuario.controller;
 
+import com.camilaester.cadastro_usuario.business.UsuarioService;
+import com.camilaester.cadastro_usuario.infrastructure.entitys.Usuario;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/usuario")
+@RequiredArgsConstructor
 public class UsuarioController {
+    private final UsuarioService usuarioService;
+
+    @PostMapping
+    public ResponseEntity<Void> salvarUsuario(@RequestBody Usuario usuario){
+        usuarioService.salvarUsuario(usuario);
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping
+    public ResponseEntity<Usuario> buscarUsuarioPorEmail(@RequestParam String email){
+        return ResponseEntity.ok(usuarioService.buscarUsuarioPorEmail(email));
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Void> deletarUsuarioPorEmail(@RequestParam String email){
+        usuarioService.deletarUsuarioPorEmail(email);
+        return ResponseEntity.ok().build();
+    }
+    @PutMapping
+    public ResponseEntity<Void> atualizarUsuarioPorId(@RequestParam Integer id,
+                                                      @RequestBody Usuario usuario){
+        usuarioService.atualizarUsuarioPorId(id, usuario); //chamada
+        return ResponseEntity.ok().build();
+    }
 }
